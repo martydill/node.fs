66a84719281c9d960c493d907a0a3f9d93824d19
Marty Dill <marty@code-ninja.org>
2012-08-19 23:47:12 +0100

    Got basic handler working

--- Node.fs.Core/Script.fsx
+++ Node.fs.Core/Script.fsx
@@ -2,7 +2,19 @@
 // for more guidance on F# programming.
 
 #load "console.fs"
+#load "http.fs"
+
 open Node.fs
+open Node.fs.Core.Http
+open Node.fs.Core.Console
 
 // Define your library scripting code here
 
+
+let handler(req:httpServerRequest, resp:httpServerResponse) =
+    resp.write("asdf") |> ignore
+    resp.endResponse
+
+let server = httpServer(handler)
+
+server.listen(9999)
--- Node.fs.Core/http.fs
+++ Node.fs.Core/http.fs
@@ -1,16 +1,58 @@
 ﻿namespace Node.fs.Core.Http
 
 
-type httpServer = class
+type httpServerRequest = class
+    
+    val req : System.Net.HttpListenerRequest
 
-    new(func) = {}
+    new(httpListenerRequest) = { req = httpListenerRequest }
+
+end
+
+
+type httpServerResponse = class
+    
+    val resp : System.Net.HttpListenerResponse
+
+    new(response) = { resp = response }
+
+    // TODO end
+    member self.endResponse = 
+        self.resp.Close()
+
+    member self.write (data:string) = 
+        printfn "Writing %s" data
+        let bytes = System.Text.ASCIIEncoding.ASCII.GetBytes(data)
+        self.resp.OutputStream.Write(bytes, 0, bytes.Length)
+
+    member self.writeHead (statusCode:int) = 
+        
+        self.resp.StatusCode = statusCode
+        
+
+end
+
+
+type httpServer(func) = class
+
+    member self.requestHandler(request, response) = 
+        func(request, response)
 
     member self.listen (port:int) = 
         let listener = new System.Net.HttpListener()
         let prefix = "http://localhost:" + port.ToString() + "/"
         listener.Prefixes.Add(prefix)
+        printfn "Listening at %s" prefix
         listener.Start()
         let context = listener.GetContext()
+        printfn "Got context"
+        self.requestHandler(new httpServerRequest(context.Request), new httpServerResponse(context.Response))
+        
+//        let bytes = System.Text.ASCIIEncoding.ASCII.GetBytes("Hello World!");
+//        
+//        context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+//        context.Response.Close();
+        //listener.Stop();
         ()
 
 end
@@ -20,6 +62,6 @@ type http = class
     new() = {}
 
     member self.createServer f (a:int) (b:int) = 
-        new httpServer()
+        new httpServer(f)
 
 end
