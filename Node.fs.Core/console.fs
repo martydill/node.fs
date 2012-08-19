7b69abdd1cbf682ac9c821f6dc2c2562d9826124
Marty Dill <marty@code-ninja.org>
2012-08-19 02:00:52 +0100

    Added the time functions

--- Node.fs.Core/console.fs
+++ Node.fs.Core/console.fs
@@ -1,9 +1,17 @@
-﻿
+﻿namespace Node.fs.Core.Console
+
+open System
+open System.Collections.Generic
+
 exception AssertionError of string
 
 // http://nodejs.org/api/stdio.html
-
 type console = class
+   
+    new() = { }
+
+    [<DefaultValue>]
+    static val mutable private MyProperty : Dictionary<string, DateTime>
 
     static member log (format : Printf.TextWriterFormat<'T>) =
         printfn format
@@ -29,6 +37,37 @@ type console = class
         
         if not expr then raise(AssertionError(msg))
         ()
+
+    static member time (label) =
+
+        let dict = match console.MyProperty with
+                    | null -> new Dictionary<string, DateTime>()
+                    | _ -> console.MyProperty
+        
+
+        console.MyProperty <- dict
+        console.MyProperty.Add(label, DateTime.Now)
+
+    static member timeEnd (label) = 
+
+        let dict = match console.MyProperty with
+                    | null -> new Dictionary<string, DateTime>()
+                    | _ -> console.MyProperty
+
+        if dict.ContainsKey(label) then 
+            
+            let endTime = DateTime.Now
+            let startTime = dict.[label]
+
+            let diff = endTime - startTime
+
+            console.log "%s: %s" label (diff.ToString())
+
+            dict.Remove(label) |> ignore
+
+    // TODO
+    static member dir (obj) = 
+        raise (AssertionError("Not implemented"))
 
 end
 
