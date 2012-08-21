e1d7668796a13845f0c3456f0111f078b61bb8ba
Marty Dill <marty@code-ninja.org>
2012-08-21 05:03:45 +0100

    Fixed url parsing

--- Node.fs.Core/url.fs
+++ Node.fs.Core/url.fs
@@ -27,26 +27,42 @@ type url = class
         let theParsedUrl = new System.Uri(urlString)
 
         { 
-            href = null;
-            protocol = null;
-            host = null;
-            auth = null;
-            hostname = null;
-            port = null;
-            pathname = null;
-            search = null;
-            path = null;
-            query = null;
-            hash = null;
+            href = theParsedUrl.OriginalString;
+            protocol = theParsedUrl.Scheme + ":";
+            host = theParsedUrl.Host + ":" + theParsedUrl.Port.ToString();
+            auth = theParsedUrl.UserInfo;
+            hostname = theParsedUrl.Host;
+            port = theParsedUrl.Port.ToString();
+            pathname = theParsedUrl.AbsolutePath;
+            search = "?" + theParsedUrl.Query.Substring(1, theParsedUrl.Query.Length - 1);
+            path = theParsedUrl.PathAndQuery;
+            query = theParsedUrl.Query.Substring(1, theParsedUrl.Query.Length - 1)
+            hash = theParsedUrl.Fragment;
          }
         
 end
 
 module tests = 
 
-    let run = 
+    let check(x, y)=
+        if x = y then
+            ()
+        else
+            printfn "**%s** = **%s**\t\tFalse" x y
+
+    do
+        
         let u = new url()
         let parsedUrl = u.parse "http://user:pass@host.com:8080/p/a/t/h?query=string#hash"
 
-        assert( parsedUrl.href = "http://user:pass@host.com:8080/p/a/t/h?query=string#hash")
-
+        check(parsedUrl.href, "http://user:pass@host.com:8080/p/a/t/h?query=string#hash")
+        check(parsedUrl.protocol, "http:")
+        check(parsedUrl.host, "host.com:8080")
+        check(parsedUrl.auth, "user:pass")
+        check(parsedUrl.hostname, "host.com")
+        check(parsedUrl.port, "8080")
+        check(parsedUrl.pathname, "/p/a/t/h")
+        check(parsedUrl.search, "?query=string")
+        check(parsedUrl.path, "/p/a/t/h?query=string")
+        check(parsedUrl.query, "query=string")
+        check(parsedUrl.hash, "#hash")
