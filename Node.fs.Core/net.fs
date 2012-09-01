417f1e0e7f66782f7dead6657304a5fa97b91ead
Marty Dill <marty@code-ninja.org>
2012-09-01 13:05:03 -0700

    Work on net listen and close

--- Node.fs.Core/net.fs
+++ Node.fs.Core/net.fs
@@ -2,12 +2,18 @@
 
 type netServer(requestHandlerFunction:System.Object) = class
      
-     member self.listen(port:int, ?host, ?backlog, ?listeningListener) =  // TODO - other parameters
-        let listener = new System.Net.Sockets.TcpListener(port)
-        listener.Start()
+    let mutable listener:System.Net.Sockets.TcpListener = null
 
+     // TODO - other parameters
+    member self.listen(port:int, ?host, ?backlog, ?listeningListener) =
+       listener <- new System.Net.Sockets.TcpListener(port)
+       listener.Start()
+    
+    member self.close = 
+        listener.Stop()
 
 end
+
 
 type net = class
 
--- Node.fs.Tests/net.netServer.Listen.fs
+++ Node.fs.Tests/net.netServer.Listen.fs
@@ -9,7 +9,7 @@ type ``Given an instance of netServer`` ()=
 
     let net = new net()
     let netServer = net.createServer(fun socket -> ())
- 
+
     [<Fact>]
     let ``listen(9999) listens on port 9999`` ()=
         netServer.listen(9999)
@@ -20,4 +20,20 @@ type ``Given an instance of netServer`` ()=
 
     interface System.IDisposable with
         member x.Dispose() = 
-            ()
+            netServer.close
+
+
+
+type ``Given a listening instance of netServer`` ()=
+
+    let net = new net()
+    let netServer = net.createServer(fun socket -> ())
+    let x = netServer.listen(9999)
+
+    [<Fact>]
+    let ``close() closes the listener`` ()= // TODO - async
+        netServer.close
+        let listeners = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners()
+        let theListener = listeners.SingleOrDefault(fun l -> l.Port = 9999)
+        theListener |> should be Null
+
