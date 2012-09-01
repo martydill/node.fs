275a6b3c3fcb1c227cc0ddff2b4b35d5969472e4
Marty Dill <marty@code-ninja.org>
2012-09-01 12:49:16 -0700

    Working on net.listen()

--- Node.fs.Core/net.fs
+++ Node.fs.Core/net.fs
@@ -2,6 +2,11 @@
 
 type netServer(requestHandlerFunction:System.Object) = class
      
+     member self.listen(port:int, ?host, ?backlog, ?listeningListener) =  // TODO - other parameters
+        let listener = new System.Net.Sockets.TcpListener(port)
+        listener.Start()
+
+
 end
 
 type net = class
---
+++ Node.fs.Tests/net.netServer.Listen.fs
@@ -0,0 +1,23 @@
+﻿module Test.``netServer - listen``
+
+open Node.fs.Core.net
+open Xunit
+open FsUnit.Xunit
+open System.Linq
+
+type ``Given an instance of netServer`` ()=
+
+    let net = new net()
+    let netServer = net.createServer(fun socket -> ())
+ 
+    [<Fact>]
+    let ``listen(9999) listens on port 9999`` ()=
+        netServer.listen(9999)
+        let listeners = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners()
+        let theListener = listeners.SingleOrDefault(fun l -> l.Port = 9999)
+        theListener |> should not' (be null)
+
+
+    interface System.IDisposable with
+        member x.Dispose() = 
+            ()
