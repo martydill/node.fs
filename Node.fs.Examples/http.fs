e083c93be42c78dbefa9ef41bb7a58982e0f735d
Marty Dill <marty@code-ninja.org>
2012-09-01 13:27:52 -0700

    Moved http demo into its own file and made program.fs run it.

---
+++ Node.fs.Examples/http.fs
@@ -0,0 +1,49 @@
+﻿module httpDemo
+
+open Node.fs.Core.Http
+open Node.fs.Core.Console
+open Node.fs.Core.Url
+
+let run = 
+    
+    let http = new http()
+    let console = new console()
+    let url = new url()
+
+    console.log "Running http demo"
+
+    http.createServer(fun (request, response) -> 
+        response.writeHead (200, dict["Content-Type", "text/html"])
+        let pathname = url.parse(request.url).pathname
+        console.log "Request for %s received." pathname
+                
+        if pathname = "/upload" then
+                    
+            request.addListener("data", fun data -> 
+                let message = sprintf "You typed %s" data 
+                response.write(message)
+            ) 
+
+            request.addListener("end", fun data -> 
+                response.endResponse
+            )
+
+        else
+
+            let data =  "<html>" +
+                        "<head>"+
+                        "</head>"+
+                        "<body>"+
+                        "<form action=\"/upload\" method=\"post\">"+
+                        "<textarea name=\"text\" rows=\"20\" cols=\"60\"></textarea>"+
+                        "<input type=\"submit\" value=\"Submit text\" />"+
+                        "</form>"+
+                        "</body>"+
+                        "</html>"
+
+            response.write data
+            response.endResponse
+
+    ).listen(8888)
+
+    console.log "The server has started"
