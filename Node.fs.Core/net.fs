db356975bd82cf808d7862981247f2bb9806e857
Marty Dill <marty@code-ninja.org>
2012-09-01 12:36:29 -0700

    Quick impl of net.createServer

---
+++ Node.fs.Core/net.fs
@@ -0,0 +1,16 @@
+﻿namespace Node.fs.Core.net
+
+type netServer(requestHandlerFunction:System.Object) = class
+     
+end
+
+type net = class
+
+    new() = {}
+
+    member self.createServer f = 
+        new netServer(f)
+
+end
+
+
---
+++ Node.fs.Tests/net.createServer.fs
@@ -0,0 +1,15 @@
+﻿module Test.``net - createServer``
+
+open Node.fs.Core.net
+open Xunit
+open FsUnit.Xunit
+
+type ``Given an instance of net`` ()=
+
+    let net = new net()
+ 
+    [<Fact>]
+    let ``createServer creates a server instance`` ()=
+      let server = net.createServer(fun socket -> ())
+      server |> should be ofExactType<netServer>
+  
