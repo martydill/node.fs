ff475560041f619ec270a1e50e2be2b136bcd9c8
Marty Dill <marty@code-ninja.org>
2012-09-05 21:10:38 -0700

    Stubbed out the path object

---
+++ Node.Tests/path.fs
@@ -0,0 +1,16 @@
+﻿module Test.``path - ``
+
+open Node.path
+open Xunit
+open FsUnit.Xunit
+
+type ``Given an instance of path`` ()=
+
+    let path = new path()
+ 
+    [<Fact>]
+    let ``join(a, b) returns a\b`` ()=
+        let result = path.join("a", "b")
+        result |> should equal "a\\b"
+
+  
--- Node/path.fs
+++ Node/path.fs
@@ -1,7 +1,32 @@
 ﻿namespace Node.path
 
+// http://nodejs.org/api/path.html
 type path = class
     
     new() = {}
 
+    member self.normalize p = 
+        ()
+
+    member self.join([<System.ParamArray>] args: string[]) = 
+        ()
+
+    member self.resolve([<System.ParamArray>] args: string[]) =
+        ()
+
+    member self.relative(from, to_) = // NAMING: Should be to
+        ()
+
+    member self.dirname p = 
+        ()
+
+    member self.basename(p, ?ext) = 
+        ()
+
+    member self.extname p = 
+        ()
+
+    member self.sep = 
+        ()
+   
 end
