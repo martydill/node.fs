5f762a2d6d9e3719ed5153b2fec605ddc2c5469d
Marty Dill <marty@code-ninja.org>
2012-10-07 23:04:24 -0700

    Added wrapping of the connection in the sqlserver_connection object. Also did a bit of cleanup.

--- Node.Libraries/node-sqlserver.fs
+++ Node.Libraries/node-sqlserver.fs
@@ -1,18 +1,34 @@
 ﻿namespace Node.Libraries
+
 open System.Data.SqlClient
 open System
 
-type sql_data_row(values) = class
-
-end
-
-type sql_result(results:sql_data_row[]) = class
-    
-    member self.length = results.Length
-
-end
-
 type sqlserver_connection(_conn) = class
+
+    member self.query(connectionString, callback, query, [<ParamArray>] args: Object[]) = 
+        self.queryRaw(connectionString, callback, query, args)
+
+    member self.queryRaw(connectionString, callback, query, [<ParamArray>] args: Object[]) = 
+             
+        use cmd = new SqlCommand(query, _conn)
+
+//        for i in 0 .. args.Length - 1 do
+//            cmd.Parameters.AddWithValue("{" + i.ToString() + "}", args.[i]) |> ignore
+        
+        let allRows = new System.Collections.Generic.List<Object[]>()
+
+        use reader = cmd.ExecuteReader()
+        while reader.Read() do
+            let rowValues:Object array = Array.zeroCreate reader.FieldCount
+            reader.GetValues(rowValues) |> ignore
+            allRows.Add(rowValues)
+
+        callback(null, allRows.ToArray())
+        
+         
+    interface System.IDisposable with
+        member x.Dispose() = 
+            _conn.Dispose()
 
 end
 
@@ -23,39 +39,23 @@ end
 type sqlserver() = class
     inherit Node.emitter.emitter()
 
-    let _conn = new SqlConnection()
-
     member self.openconnection(connectionString, callback) = // NAMING: should be open
 
         System.Threading.Tasks.Task.Factory.StartNew(fun () ->
-            _conn.ConnectionString <- connectionString
-            _conn.Open()
+            let conn = new SqlConnection()
+            conn.ConnectionString <- connectionString
+            conn.Open()
 
-            callback(null, new sqlserver_connection(_conn))
+            callback(null, new sqlserver_connection(conn))
         )
 
     member self.query(connectionString, callback, query, [<ParamArray>] args: Object[]) = 
         self.queryRaw(connectionString, callback, query, args)
     
     member self.queryRaw(connectionString, callback, query, [<ParamArray>] args: Object[]) = 
-        use conn = new SqlConnection(connectionString)
-        conn.Open()
-        use cmd = new SqlCommand(query, conn)
-
-//        for i in 0 .. args.Length - 1 do
-//            cmd.Parameters.AddWithValue("{" + i.ToString() + "}", args.[i]) |> ignore
+        self.openconnection(connectionString, fun(err, conn) ->
+            conn.queryRaw(connectionString, callback, query, args)
+        )
         
-
-        let allRows = new System.Collections.Generic.List<Object[]>()
-
-        use reader = cmd.ExecuteReader()
-        while reader.Read() do
-            let rowValues:Object array = Array.zeroCreate reader.FieldCount
-            reader.GetValues(rowValues) |> ignore
-            allRows.Add(rowValues)
-        
-       // let callback = args.[args.Length - 1] :?> FSharpFunc<System.Tuple<Object, Object[][]>, unit>
-       // callback.Invoke(new Tuple<Object, Object[][]>(null, allRows.ToArray()))
-        callback(null, allRows.ToArray())
 end
 
